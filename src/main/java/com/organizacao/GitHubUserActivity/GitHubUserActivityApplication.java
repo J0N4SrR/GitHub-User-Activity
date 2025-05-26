f7df38435e64b6d05b2602cb141e8b974ada9727
Jonas Ribeiro <jonas.ribeiro@rodipa.com.br>
2025-05-26 07:48:51 -0300

    iniciando

---
+++ src/main/java/com/organizacao/GitHubUserActivity/GitHubUserActivityApplication.java
@@ -0,0 +1,13 @@
+package com.organizacao.GitHubUserActivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubUserActivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubUserActivityApplication.class, args);
+	}
+
+}
